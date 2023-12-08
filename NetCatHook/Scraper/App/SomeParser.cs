d9c8bd43153c839aa1751191adc65cac787ec315
uncorner <denis.uncorner@gmail.com>
2023-12-08 21:02:18 +0300

    add anglesharp example parsing

--- NetCatHook/Scraper/App/SomeParser.cs
+++ NetCatHook/Scraper/App/SomeParser.cs
@@ -1,10 +1,31 @@
-﻿namespace NetCatHook.Scraper.App;
+﻿using AngleSharp;
+using AngleSharp.Dom;
+
+namespace NetCatHook.Scraper.App;
 
 class SomeParser
 {
     public string DoSome(int val)
     {
         return $"str-{val}";
+    }
+
+    public string? ParseHtml(string html)
+    {
+        //Create a new context for evaluating webpages with the default config
+        IBrowsingContext context = BrowsingContext.New(Configuration.Default);
+
+        // TODO: async
+        //Create a document from a virtual request / response pattern
+        IDocument document = context.OpenAsync(req => req.Content(html)).Result;
+
+        //Do something with LINQ
+        //IEnumerable<IElement> blueListItemsLinq = document.All.Where(m => m.LocalName == "li" && m.ClassList.Contains("blue"));
+
+        //Or directly with CSS selectors
+        IHtmlCollection<IElement> elements = document.QuerySelectorAll("a");
+
+        return elements.FirstOrDefault()?.GetAttribute("href");
     }
 
 }
