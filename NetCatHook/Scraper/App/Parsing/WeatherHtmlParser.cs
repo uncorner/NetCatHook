2a32367c801967cf2b30832d932e7839ed22ddb9
uncorner <denis.uncorner@gmail.com>
2023-12-09 22:10:54 +0300

    impl WeatherHtmlParser with test

--- NetCatHook/Scraper/App/Parsing/WeatherHtmlParser.cs
+++ NetCatHook/Scraper/App/Parsing/WeatherHtmlParser.cs
@@ -1,10 +1,10 @@
 ﻿using System.Net;
+using System.Text.RegularExpressions;
 
 namespace NetCatHook.Scraper.App.Parsing;
 
 class WeatherHtmlParser
 {
-    
     private readonly string html;
 
     public WeatherHtmlParser(string html)
@@ -12,10 +12,24 @@ class WeatherHtmlParser
         this.html = html;
     }
 
-    public int Parse()
+    public (bool success, int temp) TryParse()
     {
-        // TODO
-        throw new NotImplementedException();       
+        Regex regex = new Regex(@",""temperatureAir"":\[([-]?[0-9]+)\]",
+            RegexOptions.Multiline | RegexOptions.CultureInvariant);
+
+        // TODO: async
+        MatchCollection matches = regex.Matches(html);
+
+        var match = matches.FirstOrDefault();
+        if (match is not null)
+        {
+            var val = match.Groups[1].Value;
+            var success = int.TryParse(val, out int temp);
+
+            return (success, temp);
+        }
+
+        return (false, default(int));
     }
     
 
--- NetCatHook/ScraperTests/App/Parsing/WeatherHtmlParserTest.cs
+++ NetCatHook/ScraperTests/App/Parsing/WeatherHtmlParserTest.cs
@@ -1,23 +1,31 @@
 ﻿using NetCatHook.Scraper.App.Parsing;
 using NetCatHook.Scraper.Infrastructure;
+using Xunit.Abstractions;
 
 namespace NetCatHook.ScraperTests.App.Parsing;
 
 public class WeatherHtmlParserTest
 {
     public const string TargetUrl = "https://www.gismeteo.ru/weather-ryazan-4394/now/";
+    private readonly ITestOutputHelper output;
+
+    public WeatherHtmlParserTest(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
 
     [Fact]
-    public async Task TestParse()
+    public async Task TestTryParse()
     {
         var downloader = new HtmlDownloader();
         var html = await downloader.CallUrl(TargetUrl);
 
         var parser = new WeatherHtmlParser(html);
-        var value = parser.Parse();
+        var result = parser.TryParse();
 
-        Assert.True(value > -40 && value < 40);
+        Assert.True(result.success);
+        Assert.True(result.temp > -40 && result.temp < 40);
+        output.WriteLine(result.ToString());
     }
-
 
 }
