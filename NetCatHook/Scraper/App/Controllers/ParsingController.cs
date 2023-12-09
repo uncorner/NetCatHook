ac8d398b5b4cbc7d9aa370204027708613ac5c36
uncorner <denis.uncorner@gmail.com>
2023-12-09 17:38:50 +0300

    add sample http get method

---
+++ NetCatHook/Scraper/App/Controllers/ParsingController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCatHook.Scraper.App.Controllers;
+
+[ApiController]
+[Route("parsing")]
+public class ParsingController : ControllerBase
+{
+
+    [HttpGet("sample")]
+    public async Task<IActionResult> Sample()
+    {
+        return await Task.FromResult(Ok("Sample API response"));
+    }
+
+
+}
