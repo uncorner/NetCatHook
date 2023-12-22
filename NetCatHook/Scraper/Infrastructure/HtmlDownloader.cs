0aed13058ec7084a6d5caa428de8951764f893d5
uncorner <denis.uncorner@gmail.com>
2023-12-22 19:35:35 +0300

    html downloader is advanced, add user agent header

--- NetCatHook/Scraper/Infrastructure/HtmlDownloader.cs
+++ NetCatHook/Scraper/Infrastructure/HtmlDownloader.cs
@@ -5,13 +5,39 @@ namespace NetCatHook.Scraper.Infrastructure;
 
 class HtmlDownloader : IHtmlSource
 {
+    private static readonly List<string> userAgents = new List<string>
+        {
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 OPR/105.0.0.0 (Edition Yx 05)",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.5993.771 YaBrowser/23.11.2.771 Yowser/2.5 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0"
+        };
+
+    static HtmlDownloader()
+    {
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls13;
+    }
+
     public async Task<string> GetHtmlDataAsync(string url)
     {
-        var client = new HttpClient();
-        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls13;
-        client.DefaultRequestHeaders.Accept.Clear();
+        var httpClient = new HttpClient();
+        // Accept todo
+        httpClient.DefaultRequestHeaders.Accept.Clear();
+        httpClient.DefaultRequestHeaders.Add("User-Agent", GetRandomUserAgent());
 
-        var response = client.GetStringAsync(url);
+        var response = httpClient.GetStringAsync(url);
         return await response;
     }
+
+    private static string GetRandomUserAgent()
+    {
+        var random = new Random();
+        int randomIndex = random.Next(userAgents.Count);
+        return userAgents[randomIndex];
+    }
+
 }
