2cc76cc3b630478b175e6cd6c4688b6993d1a821
uncorner <denis.uncorner@gmail.com>
2024-01-03 15:14:52 +0300

    improving WeatherEvaluator

--- NetCatHook/Scraper/Domain/WeatherEvaluator.cs
+++ NetCatHook/Scraper/Domain/WeatherEvaluator.cs
@@ -19,6 +19,14 @@ static class WeatherEvaluator
         {
             ProcessTemperatureAir(message, data.TemperatureAir.Value);
         }
+        if (data.Pressure is not null)
+        {
+            ProcessPressure(message, data.Pressure.Value);
+        }
+        if (data.WindSpeed is not null)
+        {
+            ProcessWind(message, data.WindSpeed.Value, data.WindGust);
+        }
 
         if (message.Length > 0)
         {
@@ -28,16 +36,16 @@ static class WeatherEvaluator
         return new WeatherEvaluatorData(false, null);
     }
 
-    private static void ProcessTemperatureAir(StringBuilder message, int temperatureAir)
+    private static void ProcessTemperatureAir(StringBuilder message, int value)
     {
         StringBuilder tempInfo = new();
-        if (temperatureAir >= 1 && temperatureAir <= 2)
+        if (value >= 1 && value <= 2)
         {
             tempInfo.Append("На улице слякоть.");
         }
-        else if (temperatureAir <= -7)
+        else if (value <= -7)
         {
-            if (temperatureAir <= -15)
+            if (value <= -15)
             {
                 tempInfo.Append("На улице очень холодно, сильный мороз.");
             }
@@ -49,7 +57,60 @@ static class WeatherEvaluator
 
         if (tempInfo.Length > 0)
         {
-            tempInfo.Append($" Температура воздуха {temperatureAir} °C");
+            tempInfo.Append($" Температура воздуха {value} °C");
+            message.AppendLine(tempInfo.ToString());
+        }
+    }
+
+    private static void ProcessPressure(StringBuilder message, int value)
+    {
+        const int normalPressure = 745;
+        const int dispersion = 7;
+        StringBuilder tempInfo = new();
+        
+        if (value >= (normalPressure + dispersion))
+        {
+            tempInfo.Append($"Повышенное атмосферное давление {value} мм.р.с.");
+        }
+        if (value <= (normalPressure - dispersion))
+        {
+            tempInfo.Append($"Пониженное атмосферное давление {value} мм.р.с.");
+        }
+
+        if (tempInfo.Length > 0)
+        {
+            message.AppendLine(tempInfo.ToString());
+        }
+    }
+
+    private static void ProcessWind(StringBuilder message, int windSpeed, int? windGust)
+    {
+        StringBuilder tempInfo = new();
+        const int bound1 = 4;
+        const int bound2 = 7;
+        const int bound3 = 10;
+        var value = windSpeed;
+
+        if (value >= bound1 && value < bound2)
+        {
+            tempInfo.Append($"Ощутимый ветер, скорость {value} м.с.");
+        }
+        else if (value >= bound2 && value < bound3)
+        {
+            tempInfo.Append($"Сильный ветер, скорость {value} м.с.");
+        }
+        else if (value >= bound3)
+        {
+            tempInfo.Append($"Очень cильный ветер, скорость {value} м.с.");
+        }
+
+        if (tempInfo.Length > 0 && windGust is not null)
+        {
+            tempInfo.Append($" с порывами {windGust.Value} м.с.");
+        }
+
+        if (tempInfo.Length > 0)
+        {
             message.AppendLine(tempInfo.ToString());
         }
     }
--- NetCatHook/ScraperTests/Domain/WeatherEvaluatorTest.cs
+++ NetCatHook/ScraperTests/Domain/WeatherEvaluatorTest.cs
@@ -15,12 +15,24 @@ public class WeatherEvaluatorTest
     [Fact]
     public void TestEvaluate()
     {
-        var data = new WeatherData(true, TemperatureAir: -10);
+        var data = new WeatherData(true, TemperatureAir: -10, Pressure: 755,
+            WindSpeed: 7, WindGust: 10);
         var result = WeatherEvaluator.Evaluate(data);
 
         Assert.True(result.Processed);
         Assert.False(string.IsNullOrWhiteSpace(result.TextMessage));
         output.WriteLine(result.ToString());
+    }
+
+    [Fact]
+    public void TestEvaluateNotProcessed()
+    {
+        var data = new WeatherData(true, TemperatureAir: 20, Pressure: 745,
+            WindSpeed: 2, WindGust: null);
+        var result = WeatherEvaluator.Evaluate(data);
+
+        Assert.False(result.Processed);
+        Assert.True(string.IsNullOrWhiteSpace(result.TextMessage));
     }
 
 }
