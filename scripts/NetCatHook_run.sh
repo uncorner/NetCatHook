6288e0c898cd6a10c4e1feb31cc8df8913ead1b7
uncorner <denis.uncorner@gmail.com>
2024-06-07 17:42:09 +0300

    improve bash script

--- scripts/NetCatHook_run.sh
+++ scripts/NetCatHook_run.sh
@@ -3,4 +3,4 @@
 export ASPNETCORE_ENVIRONMENT=Development
 export ASPNETCORE_URLS=https://localhost:7171;http://localhost:5297
 
-gnome-terminal --title="NetCatHook" --command="bash -c 'cd ./net8.0; ./NetCatHook.Scraper'"
+gnome-terminal --title="NetCatHook" -- bash -c "cd ./net8.0; ./NetCatHook.Scraper; sleep 7d"
