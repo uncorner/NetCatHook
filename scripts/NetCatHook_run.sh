cf934d7acb6aa562f7fa503ecdc9467cea832999
uncorner <denis.uncorner@gmail.com>
2024-06-10 21:23:40 +0300

    add script

--- scripts/NetCatHook_run.sh
+++ scripts/NetCatHook_run.sh
@@ -3,4 +3,7 @@
 export ASPNETCORE_ENVIRONMENT=Development
 export ASPNETCORE_URLS=https://localhost:7171;http://localhost:5297
 
+cd /home/denis/development/NetCatHook_bin/
 gnome-terminal --title="NetCatHook" -- bash -c "cd ./net8.0; ./NetCatHook.Scraper; sleep 7d"
+
+#gnome-terminal --title="NetCatHook Tg Bot" -- bash -c "cd /home/denis/development/NetCatHook_bin/net8.0/; ./NetCatHook.Scraper; sleep 7d"
