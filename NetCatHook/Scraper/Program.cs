b853bbd1100ee05327446388b1496590e1976064
uncorner <denis.uncorner@gmail.com>
2025-01-30 21:14:21 +0300

    clean code

--- NetCatHook/Scraper/Program.cs
+++ NetCatHook/Scraper/Program.cs
@@ -3,29 +3,9 @@ using Microsoft.Extensions.Hosting;
 using NetCatHook.Scraper.App.HostedServices;
 using NetCatHook.Scraper.Infrastructure;
 
-//namespace NetCatHook.Scraper;
-
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddCustomServices(builder.Configuration);
 builder.Services.AddHostedService<MessengerHostedService>();
 
 var host = builder.Build();
 host.Run();
-
-//var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-//builder.Services.AddControllers();
-//builder.Services.AddCustomServices(builder.Configuration);
-
-//var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-
-//app.UseHttpsRedirection();
-
-//app.UseAuthorization();
-
-//app.MapControllers();
-
-//app.Run();
