957838a5a51356067dc848c989a9f9f3aef04546
uncorner <denis.uncorner@gmail.com>
2025-02-01 11:35:58 +0300

    some changes in starting scripts

--- scripts/NetCatHook_run.cmd
+++ scripts/NetCatHook_run.cmd
@@ -1,5 +1,4 @@
-set ASPNETCORE_ENVIRONMENT=Development
-set ASPNETCORE_URLS=https://localhost:7171;http://localhost:5297
+set DOTNET_ENVIRONMENT=Development
 
 start /B "NetCatHook" /D "net7.0" "NetCatHook.Scraper.exe"
 pause
